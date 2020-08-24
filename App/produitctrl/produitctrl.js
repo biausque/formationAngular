c72618e38f5b15afc10dce4abc5dfa459f6252d1
Biausque <david.biausque@gmail.com>
2020-08-24 15:07:32 +0200

    Pas d'utilisation de scope

--- App/produitctrl/produitctrl.js
+++ App/produitctrl/produitctrl.js
@@ -1,5 +1,9 @@
 angular.module('app').controller('produitctrl',['$scope',function ($scope){
-    $scope.ref='ABD-BCD';
-    $scope.prix=2.20;
-    $scope.nom='Beurre';
+   /*Mauvais */
+   // $scope.ref='ABD-BCD';
+   // $scope.prix=2.20;
+   // $scope.nom='Beurre';
+    this.ref='ABD-BCD';
+    this.prix=2.20;
+    this.nom='Beurre';
 }])
